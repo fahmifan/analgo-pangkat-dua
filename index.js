c6c902804f8f5512979ee665bfb56c5acf304944
miun173 <fahmifan173@gmail.com>
2019-03-03 23:29:56 +0700

    feat: add benchmarking result

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const loop = require('./loop');
 const recursive = require('./recursive');
+const faker = require('faker');
 
 function main() {
   console.log('====== loop =======');
@@ -15,6 +16,22 @@ function main() {
   console.log('221 === 4 ? %o', eval(recursive(2) === 4));
   console.log('2^3 === 8 ? %o', eval(recursive(3) === 8));
   console.log('2^4 === 16 ? %o', eval(recursive(4) === 16));
+
+  const lableLoop = 'loop';
+  const lableRecur = 'recursive';
+
+  for (let i = 0; i < 50; i++) {
+    const randNum = faker.random.number(100);
+    console.log(`\n>>> test ${i + 1} 2^${randNum}`);
+    console.time(lableLoop);
+    loop(randNum);
+    console.timeEnd(lableLoop);
+  
+    console.time(lableRecur);
+    recursive(randNum);
+    console.timeEnd(lableRecur);
+  }
+
 }
 
 main();
