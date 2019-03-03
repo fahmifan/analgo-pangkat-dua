b4b727720b0eff5367906467f673834dd4e5278a
miun173 <fahmifan173@gmail.com>
2019-03-03 23:41:13 +0700

    feat: new benchmark

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ function main() {
 
   console.log('====== recursive =======');
   console.log('2^1 === 2 ? %o', eval(recursive(1) === 2));
-  console.log('221 === 4 ? %o', eval(recursive(2) === 4));
+  console.log('2^1 === 4 ? %o', eval(recursive(2) === 4));
   console.log('2^3 === 8 ? %o', eval(recursive(3) === 8));
   console.log('2^4 === 16 ? %o', eval(recursive(4) === 16));
 
   const lableLoop = 'loop';
   const lableRecur = 'recursive';
 
-  for (let i = 0; i < 50; i++) {
-    const randNum = faker.random.number(100);
-    console.log(`\n>>> test ${i + 1} 2^${randNum}`);
+  for (let i = 0; i <= 100; i += 10) {
+    console.log(`\n>>> 2^${i}`);
+
     console.time(lableLoop);
-    loop(randNum);
+    loop(i);
     console.timeEnd(lableLoop);
   
     console.time(lableRecur);
-    recursive(randNum);
+    recursive(i);
     console.timeEnd(lableRecur);
   }
 
